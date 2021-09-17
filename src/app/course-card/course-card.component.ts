db59650ee127b726d83d60b6c5a71b8e6fe0331f
jennifer-fazler <jennifer.fazler@thermofisher.com>
2021-09-17 17:00:02 -0400

    S2.10 custom events and event emitters

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Course} from '../model/course';
 
 @Component({
@@ -11,9 +11,17 @@ export class CourseCardComponent implements OnInit {
   @Input()
   course:Course;
 
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCourseViewed() {
+    console.log("card component - button clicked ...");
+    this.courseEmitter.emit(this.course);
   }
 
 }
