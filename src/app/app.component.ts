7c9b239c7e7ab3bdb9e3e395c6110af284d66989
jennifer-fazler <jennifer.fazler@thermofisher.com>
2021-09-27 16:35:34 -0400

    S3.18 ViewChild Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
 
 @Component({
@@ -11,19 +12,11 @@ export class AppComponent {
 
   courses = COURSES;
 
-  title = COURSES[0].description;
-
-  // price = 9.99;
-  price = 9.99298283824934823;
-
-  rate = 0.67;
-
-  course = COURSES[0];
-
-  startDate = new Date(2000, 0, 1);
+  @ViewChild(CourseCardComponent)
+  card: CourseCardComponent;
 
   onCourseSelected(course:Course) {
-    console.log("App component - click event bubbled", course);
+    console.log(this.card.course.id);
   }
 
 
