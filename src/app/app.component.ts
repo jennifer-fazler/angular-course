db946454cc72e92d9a9c418a384e618b76c2efde
jennifer-fazler <jennifer.fazler@thermofisher.com>
2021-09-27 14:55:10 -0400

    S2.17 built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,17 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  // price = 9.99;
+  price = 9.99298283824934823;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
+  startDate = new Date(2000, 0, 1);
+
   onCourseSelected(course:Course) {
     console.log("App component - click event bubbled", course);
   }
