b00d99672a1c3212262e4f94050d9a38ec21cdca
jennifer-fazler <jennifer.fazler@thermofisher.com>
2021-09-28 10:02:26 -0400

    S3.20 Additional ViewChild testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
@@ -8,17 +8,40 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
 
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent;
+  @ViewChild('cardRef1', { read: ElementRef})
+  card1: CourseCardComponent;
 
-  onCourseSelected(course:Course) {
-    console.log(this.card.course.id);
+  // @ViewChild('cardRef2')
+  // card2: CourseCardComponent;
+
+  @ViewChild('container')
+  containerDiv: ElementRef;
+
+  @ViewChild('courseImage')
+  courseImage: ElementRef;
+
+
+  // constructor() {
+  //   console.log("containerDiv", this.card1);
+  // }
+
+  ngAfterViewInit() {
+    console.log("courseImage", this.courseImage)
+
+    // console.log("containerDiv", this.card1);
+
+    // avoid synchronous change to data. you can setup an async action which will not create same error.
+    // setTimeout(() => { this.courses[0].description = 'Modified after init'; });
   }
 
-
+  onCourseSelected(course:Course) {
+    console.log(this.card1);
+    // console.log(this.card2);
+    console.log("containerDiv", this.containerDiv);
+  }
 
 }
