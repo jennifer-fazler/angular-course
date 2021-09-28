ccf84f5ab7269dc563083b62065ae4158af4a8ff
jennifer-fazler <jennifer.fazler@thermofisher.com>
2021-09-28 12:55:04 -0400

    S4.22 ng-content S4.23 Content Child Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,27 +18,16 @@ import { Course } from "./model/course";
 export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
-  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  // @ViewChildren(CourseCardComponent, {read: ElementRef})
+  @ViewChildren(CourseCardComponent)
   cards: QueryList<CourseCardComponent>;
 
   constructor() {}
 
   ngAfterViewInit() {
-    // console.log(this.cards.first);
+    console.log(this.cards.first);
     this.cards.changes.subscribe((cards) => console.log(cards));
   }
 
   onCourseSelected(course: Course) {}
-
-  onCoursesEdited() {
-    this.courses.push({
-      id: 11,
-      description: "Angular Architecture Course",
-      longDescription:
-        "Learn the core RxJs Observable Pattern as well and many other Design Patterns for building Reactive Angular Applications.",
-      iconUrl:
-        "https://s3-us-west-1.amazonaws.com/angular-academy/blog/images/rxjs-reactive-patterns-small.png",
-      category: "BEGINNER",
-    });
-  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { CourseImageComponent } from '../course-image/course-image.component';
 import {Course} from '../model/course';
 
 @Component({
@@ -6,7 +7,7 @@ import {Course} from '../model/course';
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
-export class CourseCardComponent implements OnInit {
+export class CourseCardComponent implements OnInit, AfterViewInit {
 
   @Input()
   course:Course;
@@ -17,7 +18,20 @@ export class CourseCardComponent implements OnInit {
   @Output('courseSelected')
   courseEmitter = new EventEmitter<Course>();
 
+  // @ContentChild('container')  -- this will not work. scope is restricted to content part of the component
+
+  // @ContentChild('courseImage')
+  // image;
+
+  // @ContentChild(CourseImageComponent)
+  @ContentChild(CourseImageComponent, {read: ElementRef})
+  image: CourseImageComponent;
+
   constructor() { }
+
+  ngAfterViewInit() {
+    console.log(this.image);
+  }
 
   ngOnInit(): void {
   }
@@ -27,7 +41,7 @@ export class CourseCardComponent implements OnInit {
   }
 
   onCourseViewed() {
-    console.log("card component - button clicked ...");
+    // console.log("card component - button clicked ...");
     this.courseEmitter.emit(this.course);
   }
 
