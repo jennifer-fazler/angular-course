2a094af738c80c98e7e953724e75f09c3cf582a6
jennifer-fazler <jennifer.fazler@thermofisher.com>
2021-12-21 17:48:12 -0500

    S8.43 custom service - fetching data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { CoursesService } from './services/courses.service';
 
 @Component({
@@ -16,19 +15,12 @@ export class AppComponent implements OnInit {
 
   courses$ : Observable<Course[]>;
 
-  constructor(private http: HttpClient,
-              private courseService: CoursesService) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
   ngOnInit() {
-    console.log(this.courseService);
-
-    const params = new HttpParams()
-      .set("page", "1")
-      .set("pageSize", "10");
-
-    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
+    this.courses$ = this.coursesService.loadCourses();
   }
 
 
